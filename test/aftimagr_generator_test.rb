88f0f35c1ea09aca1ee74ee4a5932378655ff14c
Thomas Stankus Jr <tj@stank.us>
2008-08-05 10:22:30 -0400

    Separated --with-categories option tests into own test case. Cleaned up tests.

--- test/aftimagr_generator_test.rb
+++ test/aftimagr_generator_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/test_helper'
+require File.join(File.dirname(__FILE__), 'test_helper')
 
 class AftimagrGeneratorTest < GeneratorTestCase
   
@@ -21,32 +21,13 @@ class AftimagrGeneratorTest < GeneratorTestCase
     end
   end
   
-  def test_generates_category_names
-    # APPTODO: Fix naming with CamelCased arg, e.g., ProductLine
-    %w(product_line).each do |arg|
-      g = Rails::Generator::Base.instance('aftimagr', arg.to_a)
-      assert_equal "product_line_category", g.category_name
-      assert_equal "CreateProductLineCategories", g.categories_migration_name
-      assert_equal "product_line_categories", g.categories_table_name
-      assert_equal "product_line_categories", g.categories_controller_file_name
-    end
-  end
-  
   def test_generates_namespaced_names
-    g = Rails::Generator::Base.instance('aftimagr', %w(admin::image))
-    assert_equal 'admin::image', g.name
-  end
-  
-  def test_generates_controller
-    run_generator('aftimagr', %w(article_image))
-    assert_generated_controller_for :article_images
-  end
-  
-  def test_generates_views
-    run_generator('aftimagr', %w(article_image))
-    assert_generated_views_for :article_images, 
-        '_buttons.html.erb', '_image.html.erb', '_messages.html.erb', '_thumbnails.html.erb',
-        '_upload.html.erb', 'index.html.erb', 'new.html.erb', 'show.html.erb', 'update_js.html.erb'
+    %w(admin/image Admin::Image).each do |namespaced_string|
+      g = Rails::Generator::Base.instance('aftimagr', [namespaced_string])
+      assert_equal 'Admin::Images', g.controller_class_name
+      assert_equal ['admin'], g.controller_class_path
+      assert_equal 'images', g.plural_name
+    end
   end
   
   def test_generates_route
@@ -54,18 +35,21 @@ class AftimagrGeneratorTest < GeneratorTestCase
     assert_added_route_for :article_images
   end
   
+  def test_generates_controller
+    run_generator('aftimagr', %w(article_image))
+    assert_generated_controller_for :article_images
+  end
+  
   def test_generates_namespaced_controller
     run_generator('aftimagr', %w(admin::image))
     assert_generated_controller_for "admin::images"
   end
   
-  def test_generates_names_for_namespaced_controller
-    %w(admin/image Admin::Image).each do |namespaced_string|
-      g = Rails::Generator::Base.instance('aftimagr', [namespaced_string])
-      assert_equal 'Admin::Images', g.controller_class_name
-      assert_equal ['admin'], g.controller_class_path
-      assert_equal 'images', g.plural_name
-    end
+  def test_generates_views
+    run_generator('aftimagr', %w(article_image))
+    assert_generated_views_for :article_images, 
+        '_buttons.html.erb', '_image.html.erb', '_messages.html.erb', '_thumbnails.html.erb',
+        '_upload.html.erb', 'index.html.erb', 'new.html.erb', 'show.html.erb', 'update_js.html.erb'
   end
   
   def test_generates_model
@@ -87,47 +71,6 @@ class AftimagrGeneratorTest < GeneratorTestCase
   
   def test_does_not_generate_migration
     # APPTODO: test_does_not_generate_migration
-    # This is broken in edge rails as of 8/1/08.
-  end
-  
-  def test_generates_category_model
-    run_generator('aftimagr', %w(article_image --with-categories))
-    assert_generated_model_for :article_image_category
-  end
-  
-  def test_does_not_generate_category_model
-    run_generator('aftimagr', %w(article_image))
-    assert_skipped_model :article_image_category
-  end
-  
-  def test_generates_category_migration
-    run_generator('aftimagr', %w(article_image --with-categories))
-    assert_generated_migration :create_article_image_categories
-  end
-  
-  def test_generates_categories_controller
-    run_generator('aftimagr', %w(article_image --with-categories))
-    assert_generated_controller_for :article_image_categories
-  end
-  
-  def test_generates_categories_routes
-    run_generator('aftimagr', %w(article_image --with-categories))
-    assert_added_route_for :article_image_categories
-  end
-  
-  def test_generates_namespaced_categories_controller
-    run_generator('aftimagr', %w(admin::image --with-categories))
-    assert_generated_controller_for "admin::image_categories"
-  end
-  
-  def test_generates_names_for_namespaced_categories_controller
-    %w(admin/image Admin::Image).each do |namespaced_string|
-      g = Rails::Generator::Base.instance('aftimagr', [namespaced_string, '--with-categories'])
-      assert_equal 'Admin::ImageCategories', g.categories_controller_class_name
-      assert_equal ['admin'], g.controller_class_path
-      assert_equal 'image_categories', g.categories_table_name
-      # assert_equal "image_categories", g.categories_controller_file_name
-    end
   end
   
 end
